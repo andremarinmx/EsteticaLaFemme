6202b071e7c2a3cee2aa40405224cba882d8d1a5
André <andremarinmx.com>
2022-05-13 09:29:29 -0600

    Tabla catogoría agregada

--- database/scheme.sql
+++ database/scheme.sql
@@ -3,10 +3,26 @@ CREATE DATABASE LaFemme;
 USE LaFemme;
 
 CREATE TABLE trabajos(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(20) NOT NULL,
     descripcion VARCHAR(100) NOT NULL,
-    precio float NOT NULL
+    precio INT NOT NULL,
+    imagen VARCHAR(100),
+    id_categoria INT NOT NULL
 );
+
+CREATE TABLE categoria(
+    id_categoria INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL
+);
+
 ALTER TABLE trabajos
-    ADD imagen VARCHAR(100);
+    ADD CONSTRAINT fk_id_categoria
+    FOREIGN KEY (id_categoria)
+    REFERENCES categoria(id_categoria);
+
+CREATE TABLE usuarios(
+    id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
+    contrasena VARCHAR(20) NOT NULL
+);
